36c5fc08becb01336e3c73083c44e800bbcf8a4b
Amit Barman <amitbarrman15fifteen@gmail.com>
2022-07-24 04:21:13 +0530

    add home section and navigation bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,4 +78,58 @@ $(() => {
 	});
 });
 
+
+// home and navigation bar section js 
+function disable(){
+	$('#fir-home').css('display','none');
+	$('#sec-home').css('display','none');
+	$('#trd-home').css('display','none');
+	$('#fth-home').css('display','none');
+	$('#fifth-home').css('display','none');
+	$('#six-home').css('display','none');
+
+}
+
+$('#pri').on('click',function(){
+	disable();
+	$('#fir-home').css('display','flex');
+})
+$('#sec').on('click',function(){
+	disable();
+	$('#sec-home').css('display','flex');
+})
+$('#tri').on('click',function(){
+	disable();
+	$('#trd-home').css('display','flex');
+})
+$('#quad').on('click',function(){
+	disable();
+	$('#fth-home').css('display','flex');
+})
+$('#qint').on('click',function(){
+	disable();
+	$('#fifth-home').css('display','flex');
+})
+$('#hex').on('click',function(){
+	disable();
+	$('#six-home').css('display','flex');
+})
+
+
+$('#nav-icon').on('click',function(){
+	if($('#nav-ul').css('visibility')==='hidden'){
+		$('#nav-ul').css({
+			visibility:'visible',
+			height:'486px',
+			overflow:'hidden'
+			
+		},"slow");
+	}
+	else{
+		$('#nav-ul').css({
+			visibility:'hidden',
+			height:'0px'
+		},"slow");
+	}
+})
 // This file is Under-development
